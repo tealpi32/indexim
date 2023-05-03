34541bc1dd92100ee13bd5c883b20cf9bc74fcac
tealpi32 <80133525+tealpi32@users.noreply.github.com>
2023-05-03 22:34:56 +0300

    Create xss.php

---
+++ xss.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Hacked</title>
+  </head>
+  <body>
+    <script>alert(1);</script>
+  </body>
+</html>
